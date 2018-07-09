78596bab17f361fe9071cd6420189c50696ee88f
mkht <mkht@users.noreply.github.com>
2018-07-09 23:12:15 +0900

    Firefox ESRのインストールに対応

--- DSCResources/cFirefox/cFirefox.schema.psm1
+++ DSCResources/cFirefox/cFirefox.schema.psm1
@@ -2,34 +2,61 @@
 {
     param
     (
-        [string]
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
+        [string]
         $VersionNumber,
+
+        [Parameter()]
         [string]
         $Language = "en-US",
+
+        [Parameter()]
         [ValidateSet("x86", "x64")]
         [string]
         $MachineBits = "x86",
+
+        [Parameter()]
         [string]
         $InstallerPath,
+
+        [Parameter()]
         [string]
         $LocalPath = "$env:SystemDrive\Windows\temp\DtlDownloads\Firefox Setup " + $VersionNumber + ".exe",
+
+        [Parameter()]
         [string]
         $InstallDirectoryName,
+
+        [Parameter()]
         [string]
         $InstallDirectoryPath,
+
+        [Parameter()]
         [bool]
         $QuickLaunchShortcut = $false,
+
+        [Parameter()]
         [bool]
         $DesktopShortcut = $true,
+
+        [Parameter()]
         [bool]
         $TaskbarShortcut = $false,
+
+        [Parameter()]
         [bool]
         $MaintenanceService = $true,
+
+        [Parameter()]
         [bool]
         $StartMenuShortcuts = $true,
+
+        [Parameter()]
         [string]
         $StartMenuDirectoryName = 'Mozilla Firefox',
+
+        [Parameter()]
         [bool]
         $OptionalExtensions = $true #only Firefox 60+
     )
@@ -39,7 +66,7 @@
     if ($MachineBits -eq "x64") {
         $OS = "win64"
     }
-    else{
+    else {
         $OS = "win32"
     }
 
@@ -54,30 +81,40 @@
         ("StartMenuDirectoryName=" + $StartMenuDirectoryName),
         ("OptionalExtensions=" + $OptionalExtensions.ToString().toLower())
     )
+
     if ($InstallDirectoryName) {
         $IniContent += ("InstallDirectoryName=" + $InstallDirectoryName)
     }
+
     if ($InstallDirectoryPath) {
         $IniContent += ("InstallDirectoryPath=" + $InstallDirectoryPath)
     }
 
-    if(-not $InstallerPath){
+    if (-not $InstallerPath) {
         $InstallerPath = ("https://ftp.mozilla.org/pub/firefox/releases/{0}/{1}/{2}/Firefox%20Setup%20{0}.exe" -f $VersionNumber, $OS, $Language)
     }
 
+    if ($VersionNumber -match 'esr') {
+        $v1 = $VersionNumber.Substring(0, $VersionNumber.IndexOf('esr'))
+        $AppName = ("Mozilla Firefox {0}{1} ({2} {3})" -f $v1, ' ESR', $MachineBits, $Language)
+    }
+    else {
+        $AppName = ("Mozilla Firefox {0} ({1} {2})" -f $VersionNumber, $MachineBits, $Language)
+    }
+
+
     cApplication Install
     {
-        Name      = "Mozilla Firefox " + $VersionNumber + " (" + $MachineBits + " " + $Language + ")"
+        Name          = $AppName
         InstallerPath = $InstallerPath
-        Arguments = "-ms /INI=$IniPath"
-        PreAction = {
+        Arguments     = "-ms /INI=$IniPath"
+        PreAction     = {
             $parent = (split-path -Path $using:IniPath -Parent)
             if (-not (Test-Path -Path $parent)) {
                 New-Item -Path $parent -ItemType Directory -Force | Out-Null
             }
             $using:IniContent -join "`r`n" | Out-File -FilePath $using:IniPath -Encoding ascii -Force
         }
-        NoRestart = $true
+        NoRestart     = $true
     }
-
 }
